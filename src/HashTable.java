3ccdaff9bb040bb4ca3c526d3cc440791d62c5ba
Jakob Misbach <jakobmisbach8@gmail.com>
2020-05-05 23:18:06 -0400

    Added hashcode

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,2 +1,17 @@
 public class HashTable {
+
+    public int hash(String word){
+        final int C = 123, m = 1000;
+        int h = 0;
+
+        for(int i = 0; i < word.length(); i++){
+            h = (h * C + ord(word.charAt(i))) % m;
+        }
+
+        return h;
+    }
+
+    public int ord(char c){
+        return c;
+    }
 }
